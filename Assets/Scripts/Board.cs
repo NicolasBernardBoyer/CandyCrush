66deb24305238fed767e37a2cd5de692a7ef1dd8
NicolassBoyer <nicolasbernardboyer@gmail.com>
2024-10-23 20:06:52 -0400

    Match detecting implemented

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -62,5 +63,193 @@ public class Board : MonoBehaviour
                 
             }
         }
+        if (CheckBoard())
+        {
+            Debug.Log("Matches found, recreating board");
+            InitializeBoard();
+        } else
+        {
+            Debug.Log("No matches found, starting game");
+        }
+    }
+
+    public bool CheckBoard()
+    {
+        Debug.Log("Checking Board");
+        bool hasMatched = false;
+
+        List<Candy> candyToRemove = new();
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // check to see if node is usable
+                if (board[x, y].isUsable)
+                {
+                    //proceed to get class in node
+                    Candy candy = board[x,y].candy.GetComponent<Candy>();
+
+                    //ensure there is no match
+                    if (!candy.isMatched)
+                    {
+                        //run matching logic
+
+                        MatchResult matchedCandy = IsConnected(candy);
+
+                        if(matchedCandy.connected.Count >= 3)
+                        {
+                            // complex matching
+
+                            candyToRemove.AddRange(matchedCandy.connected);
+
+                            foreach (Candy cand in matchedCandy.connected)
+                                cand.isMatched = true;
+
+                            hasMatched = true;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return hasMatched;
+    }
+
+    //IsConnected
+    MatchResult IsConnected(Candy candy)
+    {
+        List<Candy> connected = new();
+        CandyType CandyType = candy.candyType;
+
+        connected.Add(candy);
+
+        //check right
+        CheckDirection(candy, new Vector2Int(1, 0), connected);
+        //check left
+        CheckDirection(candy, new Vector2Int(-1, 0), connected);
+
+        //have we made a match (horizontal)
+        if(connected.Count == 3)
+        {
+            Debug.Log("I have a normal horizontal match, the color is: " + connected[0].candyType);
+
+            return new MatchResult
+            {
+                connected = connected,
+                direction = MatchDirection.Horizontal
+            };
+        }
+
+        //checking for more than 3 (long horizontal)
+        else if (connected.Count > 3)
+        {
+            Debug.Log("I have a long horizontal match, the color is: " + connected[0].candyType);
+
+            return new MatchResult
+            {
+                connected = connected,
+                direction = MatchDirection.LongHorizontal
+            };
+        }
+
+        //clear out connected
+        connected.Clear();
+        //read our initial candy
+        connected.Add(candy);
+
+        //check up
+        CheckDirection(candy, new Vector2Int(0, 1), connected);
+
+        //check down
+        CheckDirection(candy, new Vector2Int(0, -1), connected);
+
+        //have we made a a 3 match (vertical)
+        if (connected.Count == 3)
+        {
+            Debug.Log("I have a normal vertical match, the color is: " + connected[0].candyType);
+
+            return new MatchResult
+            {
+                connected = connected,
+                direction = MatchDirection.Vertical
+            };
+        }
+
+        //check for more than 3 (long vertical match)
+        //have we made a a 3 match (vertical)
+        if (connected.Count > 3)
+        {
+            Debug.Log("I have a long vertical match, the color is: " + connected[0].candyType);
+
+            return new MatchResult
+            {
+                connected = connected,
+                direction = MatchDirection.LongVertical
+            };
+        } else
+        {
+            return new MatchResult
+            {
+                connected = connected,
+                direction = MatchDirection.None
+            };
+        }
+    }
+
+
+    //CheckDir
+    //Could do diagonal matches if we wanted to
+    void CheckDirection(Candy can, Vector2Int direction, List<Candy> connected)
+    {
+        CandyType type = can.candyType;
+        int x = can.xIndex + direction.x;
+        int y = can.yIndex + direction.y;
+
+        //check that we're in board boundaries
+        while (x >= 0 && x < width && y >= 0 && y < height)
+        {
+            if (board[x, y].isUsable)
+            {
+                Candy neighbourCandy = board[x, y].candy.GetComponent<Candy>();
+
+                //does it match? must not be
+                if (!neighbourCandy.isMatched && neighbourCandy.candyType == type)
+                {
+                    connected.Add(neighbourCandy);
+
+                    x += direction.x;
+                    y += direction.y;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 }
+
+public class MatchResult
+{
+    public List<Candy> connected;
+    public  MatchDirection direction;
+}
+
+public enum MatchDirection
+{
+    Vertical,
+    Horizontal,
+    LongVertical,
+    LongHorizontal,
+    Super,
+    None
+}
+
+
